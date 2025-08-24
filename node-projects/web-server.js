34e453bf431b43a695cea8ff4f9c99b52fe39725
Saurav Kumar <67901218+sauravk63@users.noreply.github.com>
2025-08-24 08:25:44 +0000

    added node server

--- node-projects/web-server.js
+++ node-projects/web-server.js
@@ -1,8 +1,26 @@
 const express = require('express')
 const app = express()
 
-app.get('/home', (req, resp)=>{
-    resp.send("Welcome to homepage!")
+app.get('/home', (req, res)=>{
+    res.send(`<html>
+            <head><title>Home</title></head>
+            <body>
+                <h1>Welcome to Homepage!</h1>
+                <p>This is a simple HTML response.</p>
+            </body>
+        </html>`)
+})
+
+app.get('/office', (req, res)=>{
+    res.json({
+        message : 'This is office page'
+    })
+})
+
+app.use((req,res)=>{
+    res.status(404).json({
+        message : "please check your url"
+    })
 })
 
 app.listen(9000, '0.0.0.0', ()=>{
